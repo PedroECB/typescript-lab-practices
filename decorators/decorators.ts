f545d2059d931fbb947e79058e375be63884cda8
Pedro Carvalho <pedrosophbc@gmail.com>
2020-08-15 20:25:25 -0300

    Decorators v2

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,7 +1,7 @@
 
+type Constructor = {new (...args: any[]): {}}
 
-
-function logarClasse(constructor:Function){
+function logarClasse(constructor:Constructor){
     console.log(constructor)
 }
 
@@ -34,3 +34,129 @@ class Eletrodomestico{
         console.log('novo...')
     }
 }
+
+
+function logarObjeto(constructor: Constructor){
+    return class extends constructor{
+        constructor(...args:any[]){
+            console.log('Antes...')
+            super(...args)
+            console.log('Depois...')
+        }
+    }
+}
+
+// Adicionando método com decorator
+
+interface FogaoInterface{
+    imprimir?():void
+}
+
+@imprimivel
+class Fogao implements FogaoInterface{
+    constructor(){
+        console.log('Novo Fogão...')
+    }
+}
+
+function imprimivel(constructor:Function){
+    constructor.prototype.imprimir = function(){
+        console.log(this)
+    }
+}
+/**
+ * O compilador do TS não consegue inferir que o método imprimir está contido dentro da classe Fogao
+ * quando o método é atribuido à classe por meio do prototype. Uma das formas de evitar isso é criando
+ * uma interface e declarando o método opcional dentro dessa interface.
+ * 
+*/
+// Interface criada para evitar que o interpretador 
+
+// new Fogao().imprimir()
+
+// Uma das formas de bular a validação do compilador
+// (<any>new Fogao()).imprimir()
+
+// const fogao = new Fogao()
+// fogao.imprimir && fogao.imprimir()
+
+
+// Desafio perfil admin
+
+const usuarioLogado = {
+    nome: 'Guilherme Filho',
+    email: 'guigui@gmail.com',
+    admin: true
+}
+
+@perfilAdmin
+class MudancaAdministrativa{
+    critico(){
+        console.log("Algo crítico foi alterado")
+    }
+}
+
+function perfilAdmin <T extends Constructor>(constructor:T){
+    return class extends constructor{
+        constructor(...args:any[]){
+            super(...args)
+            if(!usuarioLogado || !usuarioLogado.admin){
+                throw new Error('Sem permissão de administrador')
+            }
+        }
+    }
+}
+
+new MudancaAdministrativa().critico()
+
+// Decorator em métodos
+
+class ContaCorrente{
+    private saldo:number
+
+    constructor(saldoInicial:number){
+        this.saldo = saldoInicial
+    }
+
+    @congelar
+    sacar(valor:number){
+        if(valor <= this.saldo){
+            this.saldo -= valor
+            return true
+        }else{
+            return false
+        }
+    }
+
+    @congelar
+    getSaldo():string{
+        return this.saldo.toLocaleString('currency', {style: 'currency', currency:'BRL'})
+    }
+}
+
+console.log("Hello world")
+const cc = new ContaCorrente(10312.50)
+console.log(cc.getSaldo())
+cc.sacar(3500)
+console.log(cc.getSaldo())
+
+
+// Modificando o saldo diretamente propositalmente
+
+cc.getSaldo = function(){
+    return this['saldo'] + 7000
+}
+
+console.log(cc.getSaldo())
+
+function congelar(alvo:any, nomePropriedade:string, descritor:PropertyDescriptor){
+    console.log(alvo)
+    console.log(nomePropriedade)
+    descritor.writable = false
+}
+
+// const carroDeMao = {modelo:'CDM', dataFabricacao: '09/06/1990'}
+// Object.freeze(carroDeMao)
+// console.log(carroDeMao)
+// carroDeMao.modelo = 'KKK'
+// console.log(carroDeMao)
--- js/decorators/decorators.js
+++ js/decorators/decorators.js
@@ -33,3 +33,106 @@ let Eletrodomestico = class Eletrodomestico {
 Eletrodomestico = __decorate([
     decorator2({ a: 'Pedro', b: 23 })
 ], Eletrodomestico);
+function logarObjeto(constructor) {
+    return class extends constructor {
+        constructor(...args) {
+            console.log('Antes...');
+            super(...args);
+            console.log('Depois...');
+        }
+    };
+}
+let Fogao = class Fogao {
+    constructor() {
+        console.log('Novo Fogão...');
+    }
+};
+Fogao = __decorate([
+    imprimivel
+], Fogao);
+function imprimivel(constructor) {
+    constructor.prototype.imprimir = function () {
+        console.log(this);
+    };
+}
+/**
+ * O compilador do TS não consegue inferir que o método imprimir está contido dentro da classe Fogao
+ * quando o método é atribuido à classe por meio do prototype. Uma das formas de evitar isso é criando
+ * uma interface e declarando o método opcional dentro dessa interface.
+ *
+*/
+// Interface criada para evitar que o interpretador 
+// new Fogao().imprimir()
+// Uma das formas de bular a validação do compilador
+// (<any>new Fogao()).imprimir()
+// const fogao = new Fogao()
+// fogao.imprimir && fogao.imprimir()
+// Desafio perfil admin
+const usuarioLogado = {
+    nome: 'Guilherme Filho',
+    email: 'guigui@gmail.com',
+    admin: true
+};
+let MudancaAdministrativa = class MudancaAdministrativa {
+    critico() {
+        console.log("Algo crítico foi alterado");
+    }
+};
+MudancaAdministrativa = __decorate([
+    perfilAdmin
+], MudancaAdministrativa);
+function perfilAdmin(constructor) {
+    return class extends constructor {
+        constructor(...args) {
+            super(...args);
+            if (!usuarioLogado || !usuarioLogado.admin) {
+                throw new Error('Sem permissão de administrador');
+            }
+        }
+    };
+}
+new MudancaAdministrativa().critico();
+// Decorator em métodos
+class ContaCorrente {
+    constructor(saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+    sacar(valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    getSaldo() {
+        return this.saldo.toLocaleString('currency', { style: 'currency', currency: 'BRL' });
+    }
+}
+__decorate([
+    congelar
+], ContaCorrente.prototype, "sacar", null);
+__decorate([
+    congelar
+], ContaCorrente.prototype, "getSaldo", null);
+console.log("Hello world");
+const cc = new ContaCorrente(10312.50);
+console.log(cc.getSaldo());
+cc.sacar(3500);
+console.log(cc.getSaldo());
+// Modificando o saldo diretamente propositalmente
+cc.getSaldo = function () {
+    return this['saldo'] + 7000;
+};
+console.log(cc.getSaldo());
+function congelar(alvo, nomePropriedade, descritor) {
+    console.log(alvo);
+    console.log(nomePropriedade);
+    descritor.writable = false;
+}
+// const carroDeMao = {modelo:'CDM', dataFabricacao: '09/06/1990'}
+// Object.freeze(carroDeMao)
+// console.log(carroDeMao)
+// carroDeMao.modelo = 'KKK'
+// console.log(carroDeMao)
